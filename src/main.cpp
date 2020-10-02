5ed028640d279df32754c86f3ea6737a4a9ca516
Jacob Nielsen <jnillern@gmail.com>
2020-10-02 15:34:42 +0200

    Refactor HTTP

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include <mbed.h>
 #include "DHT.h"
 #include "rtos.h"
-#include "http_request.h"
-#include "network-helper.h"
+#include "EthernetInterface.h"
 
 DHT dht(A0, SEN51035P); // Temprature and Humidity sensor
+EthernetInterface net;
 
 void getTemperatureHumidity(float &temperature, float &humidity)
 {
@@ -18,42 +18,71 @@ void getTemperatureHumidity(float &temperature, float &humidity)
   humidity = dht.ReadHumidity();
 }
 
+void httpPOST(char *path, char *body)
+{
+  char sbuffer[512];
+  // Bring up the ethernet interface
+  net.connect();
+
+  // Show the network address
+  SocketAddress a;
+  net.get_ip_address(&a);
+  printf("IP address: %s\n", a.get_ip_address() ? a.get_ip_address() : "None");
+
+  // Open a socket on the network interface, and create a TCP connection to mbed.org
+  TCPSocket socket;
+  socket.open(&net);
+
+  // net.gethostbyname("163.172.146.229", &a);
+  a.set_ip_address("163.172.146.229");
+  a.set_port(1337);
+  socket.connect(a);
+  // Send a simple http request
+  printf("%s\r\n", path);
+  printf("%s\r\n", body);
+  printf("%d\r\n\r\n", sizeof body);
+
+  sprintf(sbuffer, "POST %s HTTP/1.1\r\nHost: 163.172.146.229\r\nContent-Type: application/json\r\nContent-Length: %d\r\n\r\n%s", path, sizeof body, body);
+  int scount = socket.send(sbuffer, sizeof sbuffer);
+  printf("%s\r\n", sbuffer);
+  printf("sent %d [%.*s]\n", scount, strstr(sbuffer, "\r\n") - sbuffer, sbuffer);
+
+  // Recieve a simple http response and print out the response line
+  char rbuffer[512];
+  int rcount = socket.recv(rbuffer, sizeof rbuffer);
+  printf("%s\r\n", rbuffer);
+  printf("recv %d [%.*s]\n", rcount, strstr(rbuffer, "\r\n") - rbuffer, rbuffer);
+
+  // Close the socket to return its memory and bring down the network interface
+  socket.close();
+
+  // Bring down the ethernet interface
+  net.disconnect();
+  printf("Done\n");
+}
+
 int main(void)
 {
-  NetworkInterface *network = connect_to_default_network_interface();
+  float temperature;
+  float humidity;
+  char postBody[64];
+  char postPath[32];
 
-  if (!network)
-  {
-    printf("Cannot connect to the network, see serial output\n");
-    return 1;
-  }
+  getTemperatureHumidity(temperature, humidity);
+
+  net.connect();
+  sprintf(postBody, "{\"mac\":\"%s\"}", net.get_mac_address());
+  net.disconnect();
+  sprintf(postPath, "/devices");
+
+  httpPOST(postPath, postBody);
 
   while (1)
   {
-    float temperature;
-    float humidity;
 
     getTemperatureHumidity(temperature, humidity);
 
-    printf("Temperature is %.0f \r\n", temperature);
-    printf("Humidity is %.0f \r\n", humidity);
-    {
-      HttpRequest *post_req = new HttpRequest(network, HTTP_POST, "http://httpbin.org/post");
-      post_req->set_header("Content-Type", "application/json");
-
-      const char body[] = "{\"hello\":\"world\"}";
-
-      HttpResponse *post_res = post_req->send(body, strlen(body));
-      if (!post_res)
-      {
-        printf("HttpRequest failed (error code %d)\n", post_req->get_error());
-        return 1;
-      }
-
-      printf("\n----- HTTP POST response -----\n");
-      dump_response(post_res);
-
-      delete post_req;
-    }
+    // printf("Temperature is %.0f \r\n", temperature);
+    // printf("Humidity is %.0f \r\n", humidity);
   }
 }
